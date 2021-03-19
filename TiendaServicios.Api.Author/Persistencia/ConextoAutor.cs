4e6aaf7e1f24c603d0ab7c9ec3eacd6bd9c32ad6
Jose Wigner Lliully Cruz <jwlliully@corp.fassil.com.bo>
2021-03-19 13:38:29 -0400

    Agregar archivos de proyecto.

---
+++ TiendaServicios.Api.Author/Persistencia/ConextoAutor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Author.Modelo;
+
+namespace TiendaServicios.Api.Author.Persistencia
+{
+    public class ContextoAutor : DbContext
+    {
+        public ContextoAutor(DbContextOptions<ContextoAutor> options): base(options) { }
+
+        public DbSet<AutorLibro>  AutorLibro { get; set; }
+
+
+    }
+}
