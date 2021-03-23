3a0b50595528ee9ea29e9ab0f7cc6a51c3181682
jwlliully <jwlliully@S246-A246-0-205>
2021-03-23 14:51:06 -0400

    MicroservicesCarritoCompleted

---
+++ TiendaServicios.Api.CarritoCompra/Aplicacion/Nuevo.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.CarritoCompra.Modelo;
+using TiendaServicios.Api.CarritoCompra.Persistencia;
+
+namespace TiendaServicios.Api.CarritoCompra.Aplicacion
+{
+    public class Nuevo
+    {
+        public class Ejecuta:IRequest
+        {
+            public DateTime? FechaCreacionSesion { get; set; }
+
+            public List<string> ProductoLista { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            private readonly CarritoContexto _contexto;
+            private readonly IMapper _mapper;
+
+            public Manejador(CarritoContexto contexto)
+            {
+                _contexto = contexto;
+            }
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var carritoSesion = new CarritoSesion
+                {
+                    FechaCreacion = request.FechaCreacionSesion,
+                };
+                _contexto.CarritoSesion.Add(carritoSesion);
+                var value = await _contexto.SaveChangesAsync();
+
+                if (value == 0)
+                {
+                    throw new Exception("Errores en la inserccion de carrito de compras");
+                }
+
+                int idCarrito = carritoSesion.CarritoSesionId;
+
+                foreach(var item in request.ProductoLista)
+                {
+                    var detalleSesion = new CarritoSesionDetalle
+                    {
+                        FechaCreacion = DateTime.Now,
+                        CarritoSesionId = idCarrito,
+                        ProductoSeleccionado = item
+                    };
+                    _contexto.CarritoSesionDetalle.Add(detalleSesion);
+                }
+                value = await _contexto.SaveChangesAsync();
+
+                if(value > 0)
+                {
+                    return Unit.Value;
+                }
+                throw new Exception("No se pudo registrar el detalle del carrito de compras");
+            }
+        }
+    }
+}
